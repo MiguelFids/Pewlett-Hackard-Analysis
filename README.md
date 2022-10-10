595f957b10cc505946935b4b7f668df9ef89ae73
MiguelFids <miguel.alfonso.fidelino@gmail.com>
2022-10-10 19:27:52 -0400

    Completed, probably

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Pewlett-Hackard-Analysis
 Module 7 Challenge
+Miguel Fidelino
+
+## Purpose
+    Deliverable 1: Find the number of retiring employees by title
+    Deliverable 2: The employees eligible for the mentorship 
+    
+## Results
+
+    * There are 72,458 employees that are close to retirement age.
+    * Those eligible for mentorship programs from senior employees total 1,549 possible mentees.
+    * 1090 Assistant Engineers are to retire. 
+    * The majority of staff that are close to retirement are Senior Engineers (25,916) and Regular Engineers (24,926). This makes up 49% of the total retirees. 
+
+## Summary
+How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+    Pewlett-Hackard will have an estimated 72,458 retirees. To continue normal operations, we require a total of 72,458 new hires to fill those roles. 
+
+Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+    There is a ratio of almost 1:46 mentees to mentors based off current constraints: the mentorship program are for those who have been born on 1965. There appears to be a lack of eligible employees available to be a possible mentee.
--- backup.sql
+++ backup.sql
@@ -17,7 +17,7 @@ ORDER BY e.emp_no;
 
 SELECT * FROM employees_close_to_retirement_by_title;
 
--- Use Distinct with Orderby to remove duplicate rows
+-- Use Dictinct with Orderby to remove duplicate rows
 DROP TABLE IF EXISTS employees_close_to_retirement_by_title_duplicates_removed;
 
 SELECT DISTINCT ON (ectrbt.emp_no) ectrbt.emp_no,
@@ -33,17 +33,12 @@ SELECT * FROM employees_close_to_retirement_by_title_duplicates_removed;
 
 -- Which employees are about to
 -- retire based off their job titles?
--- (GROUP BY section, step )
-DROP TABLE IF EXISTS retiring_titles;
 
 SELECT COUNT(ectrbtdr.emp_no) as emp_count,
 ectrbtdr.title
-INTO retiring_titles
 FROM employees_close_to_retirement_by_title_duplicates_removed as ectrbtdr
 GROUP BY ectrbtdr.title
 ORDER BY emp_count DESC;
-
-SELECT * FROM retiring_titles;
 
 -- DELIVERABLE 2
 DROP TABLE IF EXISTS mentorship_eligibility;
@@ -69,44 +64,15 @@ SELECT * FROM mentorship_eligibility;
 
 -- DELIVERABLE 3 QUERIES
 
--- How many of those close to retiring are still working?
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 e.first_name,
 e.last_name,
 t.title,
 t.from_date,
 t.to_date
-INTO close_to_retirement_but_still_working
 FROM employees as e
 INNER JOIN titles as t
 	ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (t.to_date = '9999-01-01')
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND NOT (to_date = '9999-01-01')
 ORDER BY e.emp_no;
 
--- Expand the mentee selection to three years instead of one.
-DROP TABLE IF EXISTS mentorship_eligibility_expanded;
-
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-e.first_name,
-e.last_name,
-e.birth_date,
-de.from_date,
-de.to_date,
-t.title
-INTO mentorship_eligibility_expanded
-FROM employees as e
-INNER JOIN dept_emp as de
-	ON e.emp_no = de.emp_no
-INNER JOIN titles as t
-	ON e.emp_no = t.emp_no
-WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01');
-
-SELECT * FROM mentorship_eligibility_expanded;
-
--- get sum of retiring_titles
-SELECT SUM(rt.emp_count) FROM retiring_titles as rt;
--- How many of those are retiring from Sr and Reg Engineers?
-SELECT SUM(rt.emp_count) 
-FROM retiring_titles as rt
-WHERE rt.title = 'Senior Engineer' OR rt.title = 'Engineer';
